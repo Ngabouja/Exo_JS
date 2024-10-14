d61a043924ce79310b163bae0b783f3c4afa538e
Ngabouja <ngabouja@edenschool.fr>
2024-10-14 14:32:08 +0200

    feat(app):add exos2 and exos3

---
+++ exo2.js
@@ -0,0 +1,5 @@
+let nombre1=Number(prompt("donner 3 numero"))
+let nombre2=Number(prompt("donner 3 numero"))
+let nombre3=Number(prompt("donner 3 numero"))
+
+console.log(nombre1+nombre2+nombre3)
---
+++ exo3.js
@@ -0,0 +1,3 @@
+let heure=prompt("Il est quelle heure??")
+
+console.log(heure*3600)
