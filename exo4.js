72202bdaba1d735ec96f57cd19027ee0a6da0fb0
Ngabouja <ngabouja@edenschool.fr>
2024-10-16 10:09:32 +0200

    feat(app)=Exo_JS

---
+++ exo4.js
@@ -0,0 +1,8 @@
+let age=prompt("Donner ton age")
+let nationalite =prompt("Donner ton pays")
+
+if (age >18 && nationalite == "français") {
+  console.log("eligible")
+} else {
+  console.log("nn eligible")
+}
---
+++ exo5.js
@@ -0,0 +1,9 @@
+let age=prompt("Donner ton age")
+
+if (age >=5 && age <=17) {
+  console.log("Réduit")
+  } else if(age <5) {
+  console.log("Gratuit")
+} else {
+  console.log("Plein tarif")
+}
